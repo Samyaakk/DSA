f5f140b333e5d6ca2c492b19a3d54b038fdca9cd
Samyaakk <samyaksharma69@gmail.com>
2025-02-01 12:18:57 +0530

    updated

---
+++ challenge.java
@@ -0,0 +1,93 @@
+// *********************************************************************************************** 
+// 30/1/25
+// search in a linked list 
+// middle of a linked list  876
+// remove ll elements 203
+// delete ll  node 237
+// delete middle on ll  2095
+// merge two sorted list 21 
+// pascal triangle 
+// set matrix zero 
+// next permutatiion 
+// maximum subarray / kadane's algo 
+// ****************************************************************************************************
+// 31/1/25
+// distance value btw two array leetcode 1385
+// palindrome reorder 
+// counting no of divisors 
+// tc<-n(log x)
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// int main(){
+//   ll n;
+//   cin>>n;
+//   vector<ll> v(1000001,0);
+//   for(ll i=1;i<=1000000;i++){
+//     for(ll j=i;j<=1000000;j+=i)v[j]++;
+//   }
+//   while(n--){
+//     ll k; cin>>k;
+//     cout<<v[k]<<endl;
+//   }
+//   return 0;
+  
+// }
+// distance value btw two array leetcode 1385
+// palindrome reorder 
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// int main(){
+//   int t;
+//   cin>>t;
+//   while(t--){
+//     ll r,c;
+//     cin>>r>>c;
+//     ll int ans;
+//     if(r>=c){
+//       ans = (r-1)*1ll*(r-1);
+//       if((r%2)==1) ans+=c;
+//       else ans+=r+(r-c);
+//     }else{
+//       ans = (c-1)*1ll*(c-1);
+//       if(c%2==0)ans+=r;
+//       else ans+=c+(c-r);
+//     }
+//     cout<<ans<<endl;
+//   }
+//   return 0;
+// }
+// tc<-n(log x)
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// int main(){
+//   ll n;
+//   cin>>n;
+//   vector<ll> v(1000001,0);
+//   for(ll i=1;i<=1000000;i++){
+//     for(ll j=i;j<=1000000;j+=i)v[j]++;
+//   }
+//   while(n--){
+//     ll k; cin>>k;
+//     cout<<v[k]<<endl;
+//   }
+//   return 0;
+// }
+//reverse of a linked list q 206 
+//remove duplicates from sorted list q83
+// palindrome linked list  q234
+// middle of linked list q2095
+// Intersection of two linked list  q160
+// Linked List cycle 1 q141
+// Linked List cycle 2 q142
+// merge Sort q148
+// q92 ,
+// q23,
+// q328
+// q1019
+// q143 
+// q2 
+// ************************************************************
+// 1/2/25
