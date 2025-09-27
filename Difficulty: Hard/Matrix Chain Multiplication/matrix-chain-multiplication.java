801e09996a9d5699b16843d09910c5af362eaa37
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-09-27 09:34:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -4,7 +4,7 @@ class Solution {
         if(dp[i][j]!=-1)return dp[i][j];
         int min = Integer.MAX_VALUE;
         for(int k=i;k<j;k++){
-            int steps = arr[i-1]*arr[k]*arr[j]+f(i,k,arr,dp)+f(k+1,j,arr,dp);
+            int steps = (arr[i-1]*arr[k]*arr[j]) + f(i,k,arr,dp)+ f(k+1,j,arr,dp);
             min = Math.min(min,steps);
         }
         return dp[i][j] = min;
@@ -21,11 +21,11 @@ class Solution {
         for(int i=n-1;i>0;i--){
             for(int j=i+1;j<n;j++){
                 int min = Integer.MAX_VALUE;
-                for(int k=i;k<j;k++){
-                    int steps = arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
-                    min = Math.min(min,steps);
-                }
-                dp[i][j]= min;
+                    for(int k=i;k<j;k++){
+                        int steps = (arr[i-1]*arr[k]*arr[j]) + dp[i][k]+dp[k+1][j];
+                        min = Math.min(min,steps);
+                    }
+                    dp[i][j] = min;
             }
         }
         return dp[1][n-1];
