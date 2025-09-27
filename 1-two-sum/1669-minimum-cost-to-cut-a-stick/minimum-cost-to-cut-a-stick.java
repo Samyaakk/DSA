2f03b9cd4fa1f9e1fd2459a1ecd8f28a1f24a0be
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-09-27 11:00:52 +0530

    Time: 11 ms (91.24%) | Memory: 42.6 MB (46.37%) - LeetSync

--- 1-two-sum/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1-two-sum/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -15,15 +15,28 @@ class Solution {
         Arrays.sort(cuts);
         int ne = cuts.length;
         int arr[] = new int[ne+2];
-        int dp[][] = new int[ne+2][ne+2];
-        for(int ar[]:dp){
-            Arrays.fill(ar,-1);
-        }
+        // int dp[][] = new int[ne+2][ne+2];
+        // for(int ar[]:dp){
+        //     Arrays.fill(ar,-1);
+        // }
         for(int i=1;i<ne+1;i++){
             arr[i] = cuts[i-1];
         }
         arr[0] = 0;
         arr[ne+1] = n; 
-        return f(1,ne,arr,dp);
+        // return f(1,ne,arr,dp);
+        int dp[][] = new int[ne+2][ne+2];
+        for(int i=ne;i>0;i--){
+            for(int j=1;j<=ne;j++){
+                if(i>j)continue;//bc 
+                int minCost = Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++){
+                    int cost = arr[j+1]-arr[i-1] + dp[i][k-1] + dp[k+1][j];
+                    minCost = Math.min(cost,minCost);
+                }
+                dp[i][j] = minCost;
+            }
+        }
+        return dp[1][ne];
     }
 }
