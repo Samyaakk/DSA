8dc52125a012e30fee78a79065ad2eb4f26e9ca4
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-09-29 09:05:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -1,15 +1,20 @@
 class Solution {
+    static int f(int i,int p,int arr[],int dp[][]){
+        if(i==arr.length)return 0;
+        if(dp[i][p+1]!=-1)return dp[i][p+1];
+        int len = 0;
+        if(p==-1 || arr[i]>arr[p]){
+             len = 1+f(i+1,i,arr,dp);
+        }
+        return dp[i][p+1] = Math.max(len,f(i+1,p,arr,dp));
+    }
     static int lis(int arr[]) {
         // code here
-        int dp[] = new int[arr.length];
-        Arrays.fill(dp,1);
-        int max=  0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<i;j++){
-                if(arr[j]<arr[i])dp[i] = Math.max(1+dp[j],dp[i]);
-            }
-            max = Math.max(dp[i],max);
+        int n = arr.length;
+        int dp[][] = new int[n][n+1];
+        for(int ar[]:dp){
+            Arrays.fill(ar,-1);
         }
-        return max;
+        return f(0,-1,arr,dp);
     }
 }
