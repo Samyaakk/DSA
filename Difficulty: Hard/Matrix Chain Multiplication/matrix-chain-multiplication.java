59ee7b3f883f8ac66dcec005222836d84cb5e3eb
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-10-06 13:17:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -1,33 +1,24 @@
 class Solution {
     static int f(int i,int j,int arr[],int dp[][]){
+        //bc
         if(i==j)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        int min = Integer.MAX_VALUE;
+        //poss
+        int mini = Integer.MAX_VALUE;
         for(int k=i;k<j;k++){
-            int steps = (arr[i-1]*arr[k]*arr[j]) + f(i,k,arr,dp)+ f(k+1,j,arr,dp);
-            min = Math.min(min,steps);
+             int count = arr[i-1]*arr[k]*arr[j]+f(i,k,arr,dp)+f(k+1,j,arr,dp);
+             mini = Math.min(mini,count);
         }
-        return dp[i][j] = min;
+        //retrun 
+        return dp[i][j] = mini;
     }
     static int matrixMultiplication(int arr[]) {
         // code here
         int n = arr.length;
-        // int dp[][] = new int[n][n];
-        // for(int ar[]:dp){
-        //     Arrays.fill(ar,-1);
-        // }
-        // return f(1,n-1,arr,dp);
-        int dp[][] = new int[n][n];
-        for(int i=n-1;i>0;i--){
-            for(int j=i+1;j<n;j++){
-                int min = Integer.MAX_VALUE;
-                    for(int k=i;k<j;k++){
-                        int steps = (arr[i-1]*arr[k]*arr[j]) + dp[i][k]+dp[k+1][j];
-                        min = Math.min(min,steps);
-                    }
-                    dp[i][j] = min;
-            }
+        int dp[][] = new int[n+1][n+1];
+        for(int ar[]:dp){
+            Arrays.fill(ar,-1);
         }
-        return dp[1][n-1];
+        return f(1,n-1,arr,dp);
     }
 }
