e72840d620e67f8e1d416e1f2c90773aa1affad6
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-10-11 12:21:33 +0530

    Time: 1 ms (78.08%) | Memory: 46.1 MB (36.93%) - LeetSync

--- 1-two-sum/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 1-two-sum/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,32 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int profit = 0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1]){
-                profit+=Math.abs(prices[i]-prices[i-1]);
-            }
+    public int f(int i,int j,int prices[],int dp[][]){
+        if(i>=prices.length)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(j==1){
+            return dp[i][j] = Math.max(-prices[i]+f(i+1,0,prices,dp),f(i+1,1,prices,dp));
+        }else{
+            return dp[i][j] = Math.max(prices[i]+f(i+1,1,prices,dp),f(i+1,0,prices,dp));
         }
-        return profit;
+    }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        // int dp[][] = new int[n][2];
+        // for(int arr[]:dp){
+        //     Arrays.fill(arr,-1);
+        // }
+        // return f(0,1,prices,dp);
+        int next[] = new int[2];
+        for(int i=n-1;i>=0;i--){
+            int curr[] = new int[2];
+            for(int j=0;j<2;j++){
+                if(j==1){
+                    curr[j] = Math.max(-prices[i]+next[0],next[1]);
+                }else{
+                    curr[j] = Math.max(prices[i]+next[1],next[0]);
+                }
+            }
+            next = curr;
+        }
+        return next[1];
     }
 }
