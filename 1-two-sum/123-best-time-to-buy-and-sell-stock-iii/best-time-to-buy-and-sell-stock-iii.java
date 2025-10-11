82038d4951be2b293c86ee35fb19ad55c7fd7f0c
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-10-11 12:32:15 +0530

    Time: 85 ms (26.73%) | Memory: 76 MB (24.07%) - LeetSync

--- 1-two-sum/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 1-two-sum/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,20 @@
 class Solution {
-    public int f(int i,int tr,int arr[],int dp[][]){
-        if(i==arr.length || tr==4)return 0;
-        if(dp[i][tr]!=-1)return dp[i][tr];
-        if(tr%2==0){
-            return dp[i][tr] = Math.max(-arr[i]+f(i+1,tr+1,arr,dp),f(i+1,tr,arr,dp));
+    public int f(int i,int j,int prices[],int dp[][]){
+        if(i>=prices.length)return 0;
+        if(j>=4)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(j%2==0){
+            return dp[i][j] = Math.max(-prices[i]+f(i+1,j+1,prices,dp),f(i+1,j,prices,dp));
         }else{
-            return dp[i][tr] = Math.max(arr[i]+f(i+1,tr+1,arr,dp),f(i+1,tr,arr,dp));
+            return dp[i][j] = Math.max(prices[i]+f(i+1,j+1,prices,dp),f(i+1,j,prices,dp));
         }
     }
-    public int maxProfit(int[] arr) {
-        int n = arr.length;
-        // int dp[][] = new int[n][4];
-        // for(int ar[]:dp){
-        //     Arrays.fill(ar,-1);
-        // }
-        // return f(0,0,arr,dp);
-        int dp[][] = new int[n+1][5];
-        for(int i=n-1;i>=0;i--){
-            for(int tr=3;tr>=0;tr--){
-                if(tr%2==0){
-                    dp[i][tr] = Math.max(-arr[i]+dp[i+1][tr+1],dp[i+1][tr]);
-                }else{
-                    dp[i][tr] = Math.max(arr[i]+dp[i+1][tr+1],dp[i+1][tr]);
-                }
-            }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][] = new int[n][4];
+        for(int arr[]:dp){
+            Arrays.fill(arr,-1);
         }
-        return dp[0][0];
-        // you can do space optimizaiton yr 
+        return f(0,0,prices,dp);
     }
 }
