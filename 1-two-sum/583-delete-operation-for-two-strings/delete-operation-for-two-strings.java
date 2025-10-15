6f737a92125e421c7a9b7d669e6ba4a1d438e5b5
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-10-15 10:44:45 +0530

    Time: 6 ms (92.67%) | Memory: 44.7 MB (73.33%) - LeetSync

--- 1-two-sum/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 1-two-sum/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -2,23 +2,20 @@ class Solution {
     public int f(int i,int j,String s1,String s2,int dp[][]){
         if(i<0 || j<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        int take = 0;
-        int notTake  =0;
         if(s1.charAt(i)==s2.charAt(j)){
-            take = 1+f(i-1,j-1,s1,s2,dp);
+            return dp[i][j] = 1+f(i-1,j-1,s1,s2,dp);
         }else{
-            notTake = Math.max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
+            return dp[i][j] = Math.max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
         }
-        return dp[i][j] = Math.max(take,notTake);
     }
-    public int minDistance(String s1, String s2) {
-        int n = s1.length();
-        int m  =s2.length();
-        int dp[][] = new int[n+1][m+1];
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int dp[][] = new int[n][m];
         for(int arr[]:dp){
             Arrays.fill(arr,-1);
         }
-        int sizeOfLcs = f(n-1,m-1,s1,s2,dp);
-        return n+m-2*sizeOfLcs;
+        int lcs = f(n-1,m-1,word1,word2,dp);
+        return n+m-(2*lcs);
     }
 }
