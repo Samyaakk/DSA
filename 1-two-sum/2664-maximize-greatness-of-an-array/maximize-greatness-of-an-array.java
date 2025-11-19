6df1f4bc18667c07e19fad0c44e55d3a3f21ae5c
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-11-19 10:38:51 +0530

    Time: 19 ms (20.78%) | Memory: 75.7 MB (17.85%) - LeetSync

--- 1-two-sum/2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.java
+++ 1-two-sum/2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.java
@@ -1,25 +1,17 @@
 class Solution {
     public int maximizeGreatness(int[] nums) {
-        int n = nums.length;
-        int arr[] = new int[n];
         Arrays.sort(nums);
-        int k = n-1;
-        int w = 0;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]==nums[k]){
-                continue;
+        int i = 0;
+        int j = 0;
+        int c = 0;
+        int n = nums.length;
+        while(i<n && j<n){
+            if(nums[j]>nums[i]){
+                i++;
+                j++;
+                c++;
             }else{
-                arr[i]=nums[k--];
-            }
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
-        int c= 0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>0){
-                // break;
-            c++;
+                j++;
             }
         }
         return c;
