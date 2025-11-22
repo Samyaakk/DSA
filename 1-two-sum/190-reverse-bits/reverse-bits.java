30823a2b0349d1deb0d6f0b45dc7096c90d6e38b
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-11-22 13:18:50 +0530

    Time: 1 ms (32.49%) | Memory: 42.6 MB (10.34%) - LeetSync

--- 1-two-sum/190-reverse-bits/reverse-bits.java
+++ 1-two-sum/190-reverse-bits/reverse-bits.java
@@ -2,32 +2,37 @@ class Solution {
     public int reverseBits(int n) {
         int c=0;
         int no = n;
-        while(no>0){
-            c++;
-            no/=2;
-        }
-        no = n;
-        int arr[] = new int[32];
-        int k = 0;
-        while(no>0){
-            if(no%2==1){
-                arr[k++]=1;
-            }else{
-                arr[k++]=0;
-            }
-            no/=2;
-        }
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        k=0;
+        int k = 31;
         int res = 0;
-        for(int x=arr.length-1;x>=0;x--){
-            if(arr[x]==1){
-                res+=(int)Math.pow(2,k);
+        while(no>0){
+            if((no&1)==1){
+                res+=Math.pow(2,k);
             }
-            k++;
+            no>>=1;
+            k--;
         }
+        // no = n;
+        // int arr[] = new int[32];
+        // int k = 0;
+        // while(no>0){
+        //     if(no%2==1){
+        //         arr[k++]=1;
+        //     }else{
+        //         arr[k++]=0;
+        //     }
+        //     no/=2;
+        // }
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+        // k=0;
+        // int res = 0;
+        // for(int x=arr.length-1;x>=0;x--){
+        //     if(arr[x]==1){
+        //         res+=(int)Math.pow(2,k);
+        //     }
+        //     k++;
+        // }
         return res;
     }
 }
