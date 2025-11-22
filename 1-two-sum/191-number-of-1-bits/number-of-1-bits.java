bc01706465ca47f6e37eb9ed2b88f7b960df4f31
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-11-22 12:49:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (24.03%) - LeetSync

--- 1-two-sum/191-number-of-1-bits/number-of-1-bits.java
+++ 1-two-sum/191-number-of-1-bits/number-of-1-bits.java
@@ -1,11 +1,17 @@
 class Solution {
     public int hammingWeight(int n) {
-        int no = n;
-        int c = 0;
-        while(no>0){
-            if(no%2==1)c++;
-            no/=2;
+        int c=0;
+        while(n>0){
+            if((n&1)==1)c++;
+            n>>=1;
         }
         return c;
+        // int no = n;
+        // int c = 0;
+        // while(no>0){
+        //     if(no%2==1)c++;
+        //     no/=2;
+        // }
+        // return c;
     }
 }
