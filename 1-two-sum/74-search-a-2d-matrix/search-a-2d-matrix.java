4006b9a7aa7eda5f2889309b867c5959b6942412
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-11-23 17:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (16.85%) - LeetSync

--- 1-two-sum/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 1-two-sum/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,14 +3,14 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         int i = 0;
-        int j = m-1;
-        while(i<n && j>=0){
-            if(matrix[i][j]==target){
-                return true;
-            }else if(matrix[i][j]>target){
-                j--;
+        int j = (m*n)-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(matrix[mid/m][mid%m]==target)return true;
+            else if(matrix[mid/m][mid%m]>target){
+                j = mid-1;
             }else{
-                i++;
+                i = mid+1;
             }
         }
         return false;
