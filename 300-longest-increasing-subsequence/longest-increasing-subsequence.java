b1da812dc821e19714e74820abb03054cabf6cc3
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-12-21 14:30:38 +0530

    Time: 82 ms (21.94%) | Memory: 169.6 MB (16.93%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,32 @@
 class Solution {
-    public int f(int i,int prev,int arr[],int dp[][]){
-        if(i<0)return 0;
-        int take = 0;
-        int notTake = 0;
-        if(prev!=-1 && dp[i][prev]!=-1)return dp[i][prev];
-        if(prev == -1||arr[i]<arr[prev]){
-            take = Math.max(1+f(i-1,i,arr,dp),f(i-1,prev,arr,dp));
-        }else{
-            notTake = f(i-1,prev,arr,dp);
+    public int lcs(int arr1[],int arr2[]){
+        int n = arr1.length;
+        int m = arr2.length;
+        int dp[][] = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int take = 0;
+                int notTake = 0;
+                if(arr1[i-1]==arr2[j-1]){
+                    take = 1+dp[i-1][j-1];
+                }else{
+                    notTake = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+                dp[i][j] = Math.max(take,notTake);
+            }
         }
-        if(prev==-1)prev=0;
-        return dp[i][prev] = Math.max(take,notTake);
+        return dp[n][m];
     }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[][] = new int[n+1][n+1];
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums)set.add(i);
+        int arr[] = new int[set.size()];
+        int k = 0;
+        for(int i:set){
+            arr[k++]=i;
         }
-        return f(n-1,-1,nums,dp);
+        Arrays.sort(arr);
+        return lcs(nums,arr);
     }
 }
