203a20fcb1875ab246875c5e76c0459b7cb6598c
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-12-21 17:17:31 +0530

    Time: 21 ms (41.06%) | Memory: 94.4 MB (31.63%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,39 +1,25 @@
 class Solution {
-    public int f(int i,int b,int c,int arr[],int dp[][][]){
-        if(i==arr.length)return 0;
-        if(c==2)return 0;
-        if(dp[i][b][c]!=-1)return dp[i][b][c];
-        if(b==0){
-            return dp[i][b][c] = Math.max(-arr[i]+f(i+1,1,c,arr,dp),f(i+1,0,c,arr,dp));
+    public int f(int i,int tr,int arr[]){
+        if(i==arr.length||tr==4)return 0;
+        if(tr%2==0){
+            return Math.max(-arr[i]+f(i+1,tr+1,arr),f(i+1,tr,arr));
         }else{
-            return dp[i][b][c] = Math.max(arr[i]+f(i+1,0,c+1,arr,dp),f(i+1,1,c,arr,dp));
+            return Math.max(arr[i]+f(i+1,tr+1,arr),f(i+1,tr,arr));
         }
     }
     public int maxProfit(int[] prices) {
+        // return f(0,0,prices);
         int n = prices.length;
-        int dp[][][] = new int[n][2][2];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                for(int k=0;k<2;k++){
-                    dp[i][j][k] = -1;
+        int dp[][] = new int[n+1][5];
+        for(int i=n-1;i>=0;i--){
+            for(int j=3;j>=0;j--){
+                if(j%2==0){
+                    dp[i][j] = Math.max(-prices[i]+dp[i+1][j+1],dp[i+1][j]);
+                }else{
+                    dp[i][j] = Math.max(prices[i]+dp[i+1][j+1],dp[i+1][j]);
                 }
             }
         }
-        return f(0,0,0,prices,dp);
+        return dp[0][0];
     }
 }
-// class Solution {
-//     public int f(int i,int b,int k,int arr[]){
-//         if(i==arr.length)return 0;
-//         if(k==0)return 0;
-//         if(b==0){
-//             return Math.max(-arr[i]+f(i+1,1,k,arr),f(i+1,0,k,arr));
-//         }else{
-//             k--;
-//             return Math.max(arr[i]+f(i+1,0,k,arr),f(i+1,1,k,arr));
-//         }
-//     }
-//     public int maxProfit(int[] prices) {
-//         return f(0,0,2,prices);
-//     }
-// }
