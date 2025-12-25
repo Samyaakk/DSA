1b61586f343d59de35c3852ba24deb477dc43f82
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-12-25 13:44:15 +0530

    Time: 2521 ms (5.07%) | Memory: 41.8 MB (72.22%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int f(int n){
+        if(n==0||n==1)return 1;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans+=f(i)*f(n-i-1);
+        }
+        return ans;
+    }
+    public int numTrees(int n) {
+        return f(n);
+    }
+}
