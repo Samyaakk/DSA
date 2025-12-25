85a4789e239cbd90ae2e5459ad2ccd11444fdcc5
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-12-25 13:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (72.22%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -8,6 +8,17 @@ class Solution {
         return ans;
     }
     public int numTrees(int n) {
-        return f(n);
+        // return f(n);
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            int ans = 0;
+            for(int j=0;j<i;j++){
+                ans += dp[j]*dp[i-j-1];
+            }
+            dp[i] = ans;
+        }
+        return dp[n];
     }
 }
