79c97e7a5b723554bfc2329de112f6a550c29bf1
Samyaakk <122391739+Samyaakk@users.noreply.github.com>
2025-12-25 14:58:40 +0530

    Time: 3 ms (77.88%) | Memory: 47.5 MB (27.40%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,44 +1,30 @@
 class Solution {
     public int longestMountain(int[] arr) {
         int n = arr.length;
-        if(n<3)return 0;
-        int ltr[] = new int[n];
-        int rtl[] = new int[n];
-        ltr[0] = 1;
-        int x= 0;
-        int y= 0;
-        for(int i=1;i<n;i++){
-            if(arr[i-1]<arr[i]){
-                x++;
-                ltr[i] = ltr[i-1]+1;
+        boolean uph  = false;
+        boolean dh = false;
+        int i=0;
+        int max =0;
+        while(i<n){
+            int start = i;
+            if(i!=n-1 && arr[i]<arr[i+1]){
+                while(i<n-1 && arr[i]<arr[i+1]){
+                    uph=true;
+                    i++;
+                }
+                while(i<n-1 && arr[i]>arr[i+1]){
+                    dh=true;
+                    i++;
+                }
+                if(uph && dh){
+                    max = Math.max(max,i-start+1);
+                }
+                uph=false;
+                dh=false;
             }else{
-                ltr[i] = 1;
+                i++;
             }
         }
-        rtl[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            if(arr[i]>arr[i+1]){
-                y++;
-                rtl[i] = rtl[i+1]+1;
-            }else{
-                rtl[i] = 1;
-            }
-        }
-        int max = 0;
-        for(int i=0;i<n;i++){
-            System.out.print(ltr[i]+" ");
-        }
-        System.out.println();
-        for(int j=0;j<n;j++){
-            System.out.print(rtl[j]+" ");
-        }
-        for(int i=1;i<n-1;i++){
-            if(ltr[i]>1&&rtl[i]>1){
-                max = Math.max(max,rtl[i]+ltr[i]);
-            }
-        }
-        if(max==2||max==1||max==0)return 0;
-        if(x==0||y==0)return 0;
-        return max-1;
+        return max;
     }
 }
